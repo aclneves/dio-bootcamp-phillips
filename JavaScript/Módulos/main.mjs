fb2904da1b1fc104b39d8925efc99eb8b0a351bb
aclneves <aclneves@gmail.com>
2022-04-05 18:30:48 -0300

    Finalizado o curso de módulos em JS

---
+++ JavaScript/Módulos/main.mjs
@@ -0,0 +1,5 @@
+import {mostraIdade, mostraCidade, mostraHobby} from "./funcoes.mjs";
+
+console.log(mostraIdade("Antonio", 34));
+console.log(mostraCidade("Antonio", "Rio de Janeiro"));
+console.log(mostraHobby("Antonio", "Jogar Tenis"));
